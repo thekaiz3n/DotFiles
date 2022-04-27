4bfc3a5c1d1f9b38904aa931cfa87028240debc8
thekaiz3n <thekaiz3n@thekaiz3n.localdomain>
2022-04-27 16:57:02 -0300

    Changing hacktools min

--- distros_setup/hacktools_min.sh
+++ distros_setup/hacktools_min.sh
@@ -14,24 +14,12 @@ echo "Install fff"
 go install github.com/tomnomnom/fff@latest
 echo "Install hakrawler"
 go install github.com/hakluke/hakrawler@latest
-echo "Install tojson"
-go install github.com/tomnomnom/hacks/tojson@latest
 echo "Install gowitness"
 go install github.com/sensepost/gowitness@latest
-echo "Install rush"
-go install github.com/shenwei356/rush@latest
 echo "Install naabu"
 go install github.com/projectdiscovery/naabu/cmd/naabu@latest
 echo "Install hakcheckurl"
 go install github.com/hakluke/hakcheckurl@latest
-echo "Install shuffledns"
-go install github.com/projectdiscovery/shuffledns/cmd/shuffledns@latest
-echo "Install rescope"
-go install github.com/root4loot/rescope@latest
-echo "Install gron"
-go install github.com/tomnomnom/gron@latest
-echo "Install html-tool"
-go install github.com/tomnomnom/hacks/html-tool@latest
 echo "Install Chaos"
 go install github.com/projectdiscovery/chaos-client/cmd/chaos@latest
 echo "Install gf"
@@ -46,8 +34,6 @@ echo "Install assetfinder"
 go install github.com/tomnomnom/assetfinder@latest
 echo "Install github-subdomains"
 go install github.com/gwen001/github-subdomains@latest
-echo "Install cf-check"
-go install github.com/dwisiswant0/cf-check@latest
 echo "Install waybackurls"
 go install github.com/tomnomnom/hacks/waybackurls@latest
 echo "Install nuclei"
@@ -56,9 +42,6 @@ echo "Install anew"
 go install github.com/tomnomnom/anew@latest
 echo "Install notify"
 go install github.com/projectdiscovery/notify/cmd/notify@latest
-echo "Install mildew"
-go install github.com/daehee/mildew/cmd/mildew@latest
-echo "Install dirdar"
 go install github.com/m4dm0e/dirdar@latest
 echo "Install unfurl"
 go install github.com/tomnomnom/unfurl@latest
@@ -78,32 +61,16 @@ echo "Install Gxss"
 go install github.com/KathanP19/Gxss@latest
 echo "Install gospider"
 go install github.com/jaeles-project/gospider@latest
-echo "Install crobat"
-go install github.com/cgboal/sonarsearch/crobat@latest
-echo "Install crlfuzz"
-go install github.com/dwisiswant0/crlfuzz/cmd/crlfuzz@latest
 echo "Install dalfox"
 go install github.com/hahwul/dalfox/v2@latest
 echo "Install puredns"
 go install github.com/d3mondev/puredns/v2@latest
-echo "Install resolveDomains"
-go install github.com/Josue87/resolveDomains@latest
-echo "Install interactsh-client"
-go install github.com/projectdiscovery/interactsh/cmd/interactsh-client@latest
-echo "Install gotator"
-go install github.com/Josue87/gotator@latest
-echo "Install kxss"
-go install github.com/tomnomnom/hacks/kxss@latest
 echo "Install GetJs"
 go install github.com/003random/getJS@latest
-echo "Install Goop"
-go install github.com/deletescape/goop@latest
 echo "Install Meg"
 go install github.com/tomnomnom/meg@latest
 echo "Install Freq"
 go install github.com/takshal/freq@latest
-echo "Install Sdlookup"
-go install github.com/j3ssie/sdlookup@latest
 echo "Install Sigurlfinder"
 go install -v github.com/signedsecurity/sigurlfind3r/cmd/sigurlfind3r@latest
 echo "Install Chromedp"
@@ -117,16 +84,10 @@ declare -A repos
 repos["gf"]="tomnomnom/gf"
 repos["Gf-Patterns"]="1ndianl33t/Gf-Patterns"
 repos["LinkFinder"]="dark-warlord14/LinkFinder"
-repos["Interlace"]="codingo/Interlace"
 repos["JSScanner"]="0x240x23elu/JSScanner"
 repos["GitTools"]="internetwache/GitTools"
 repos["SecretFinder"]="m4ll0k/SecretFinder"
 repos["M4ll0k"]="m4ll0k/BBTz"
-repos["Git-Dumper"]="arthaud/git-dumper"
-repos["CORStest"]="RUB-NDS/CORStest"
-repos["Knock"]="guelfoweb/knock"
-repos["Photon"]="s0md3v/Photon"
-repos["Sudomy"]="screetsec/Sudomy"
 repos["DNSvalidator"]="vortexau/dnsvalidator"
 repos["Massdns"]="blechschmidt/massdns"
 
