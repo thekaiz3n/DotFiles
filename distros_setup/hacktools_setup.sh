94655ee6bba407c3aeeb25bceb6bf3bf1166d237
thekaiz3n <douglasdullius1@gmail.com>
2022-04-26 22:01:10 -0300

    Update

--- distros_setup/hacktools_setup.sh
+++ distros_setup/hacktools_setup.sh
@@ -3,114 +3,58 @@
 DEBUG_STD="&>/dev/null"
 DEBUG_ERROR="2>/dev/null"
 
-# TERM COLORS
-bblue='\033[1;34m'
-
-printf "${bblue} Running: Installing Golang tools (${#gotools[@]})${reset}\n\n"
-
 go env -w GO111MODULE=auto
 
-echo "Install fff"
 go install github.com/tomnomnom/fff@latest
-echo "Install hakrawler"
 go install github.com/hakluke/hakrawler@latest
-echo "Install tojson"
 go install github.com/tomnomnom/hacks/tojson@latest
-echo "Install gowitness"
 go install github.com/sensepost/gowitness@latest
-echo "Install rush"
 go install github.com/shenwei356/rush@latest
-echo "Install naabu"
 go install github.com/projectdiscovery/naabu/cmd/naabu@latest
-echo "Install hakcheckurl"
 go install github.com/hakluke/hakcheckurl@latest
-echo "Install shuffledns"
 go install github.com/projectdiscovery/shuffledns/cmd/shuffledns@latest
-echo "Install rescope"
 go install github.com/root4loot/rescope@latest
-echo "Install gron"
 go install github.com/tomnomnom/gron@latest
-echo "Install html-tool"
 go install github.com/tomnomnom/hacks/html-tool@latest
-echo "Install Chaos"
 go install github.com/projectdiscovery/chaos-client/cmd/chaos@latest
-echo "Install gf"
 go install github.com/tomnomnom/gf@latest
-echo "Install qsreplace"
 go install github.com/tomnomnom/qsreplace@latest
-echo "Install Amass"
 go install github.com/OWASP/Amass/v3/...@latest
-echo "Install ffuf"
 go install github.com/ffuf/ffuf@latest
-echo "Install assetfinder"
 go install github.com/tomnomnom/assetfinder@latest
-echo "Install github-subdomains"
 go install github.com/gwen001/github-subdomains@latest
-echo "Install cf-check"
 go install github.com/dwisiswant0/cf-check@latest
-echo "Install waybackurls"
 go install github.com/tomnomnom/hacks/waybackurls@latest
-echo "Install nuclei"
 go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
-echo "Install anew"
 go install github.com/tomnomnom/anew@latest
-echo "Install notify"
 go install github.com/projectdiscovery/notify/cmd/notify@latest
-echo "Install mildew"
 go install github.com/daehee/mildew/cmd/mildew@latest
-echo "Install dirdar"
 go install github.com/m4dm0e/dirdar@latest
-echo "Install unfurl"
 go install github.com/tomnomnom/unfurl@latest
-echo "Install httpx"
 go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
-echo "Install github-endpoints"
 go install github.com/gwen001/github-endpoints@latest
-echo "Install dnsx"
 go install github.com/projectdiscovery/dnsx/cmd/dnsx@latest
-echo "Install subfinder"
 go install github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
-echo "Install gauplus"
 go install github.com/bp0lr/gauplus@latest
-echo "Install subjs"
 go install github.com/lc/subjs@latest
-echo "Install Gxss"
 go install github.com/KathanP19/Gxss@latest
-echo "Install gospider"
 go install github.com/jaeles-project/gospider@latest
-echo "Install crobat"
-go install github.com/cgboal/sonarsearch/crobat@latest
-echo "Install crlfuzz"
+go get github.com/cgboal/sonarsearch/cmd/crobat
 go install github.com/dwisiswant0/crlfuzz/cmd/crlfuzz@latest
-echo "Install dalfox"
 go install github.com/hahwul/dalfox/v2@latest
-echo "Install puredns"
 go install github.com/d3mondev/puredns/v2@latest
-echo "Install resolveDomains"
 go install github.com/Josue87/resolveDomains@latest
-echo "Install interactsh-client"
 go install github.com/projectdiscovery/interactsh/cmd/interactsh-client@latest
-echo "Install gotator"
 go install github.com/Josue87/gotator@latest
-echo "Install kxss"
 go install github.com/tomnomnom/hacks/kxss@latest
-echo "Install GetJs"
 go install github.com/003random/getJS@latest
-echo "Install Goop"
 go install github.com/deletescape/goop@latest
-echo "Install Meg"
 go install github.com/tomnomnom/meg@latest
-echo "Install Freq"
 go install github.com/takshal/freq@latest
-echo "Install Sdlookup"
 go install github.com/j3ssie/sdlookup@latest
-echo "Install Sigurlfinder"
 go install -v github.com/signedsecurity/sigurlfind3r/cmd/sigurlfind3r@latest
-echo "Install Chromedp"
 go install github.com/chromedp/chromedp@latest
-echo "Install Airixss"
 go install github.com/ferreiraklet/airixss@latest
-echo "Install Nilo"
 go install github.com/ferreiraklet/nilo@latest
 
 declare -A repos
@@ -143,8 +87,6 @@ pip3 install uro
 
 eval wget -nc -O ~/.gf/potential.json https://raw.githubusercontent.com/devanshbatham/ParamSpider/master/gf_profiles/potential.json $DEBUG_STD
 
-printf "${bblue}\n Running: Installing repositories (${#repos[@]})${reset}\n\n"
-
 cd "$dir" || {
     echo "Failed to cd to $dir in ${FUNCNAME[0]} @ line ${LINENO}"
     exit 1
@@ -158,11 +100,7 @@ for repo in "${!repos[@]}"; do
     eval cd $dir/$repo $DEBUG_STD
     eval git pull $DEBUG_STD
     exit_status=$?
-    if [ $exit_status -eq 0 ]; then
-        printf "${yellow} $repo installed (${repos_step}/${#repos[@]})${reset}\n"
-    else
-        printf "${red} Unable to install $repo, try manually (${repos_step}/${#repos[@]})${reset}\n"
-    fi
+    
     if [ -s "requirements.txt" ]; then
         eval $SUDO pip3 install -r requirements.txt $DEBUG_STD
     fi
