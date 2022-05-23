5bb5b7f18f1224706095299e9492ced8730a1365
thekaiz3n <thekaiz3n@thekaiz3n.localdomain>
2022-05-23 17:37:24 -0300

    sb files

--- distros_setup/archlinux/terminal.sh
+++ distros_setup/archlinux/terminal.sh
@@ -6,8 +6,8 @@ cp  ~/downloads/DotFiles/config/zsh/.zshrc ~/.config/zsh/.zshrc
 source ~/.config/zsh/.zshrc
 
 echo "Installing comic-mono fonts"
-mkdir /usr/share/fonts/comic
+sudo mkdir /usr/share/fonts/comic
 cd /usr/share/fonts/comic
-curl -O https://dtinth.github.io/comic-mono-font/ComicMono.ttf
-curl -O https://dtinth.github.io/comic-mono-font/ComicMono-Bold.ttf
+sudo curl -O https://dtinth.github.io/comic-mono-font/ComicMono.ttf
+sudo curl -O https://dtinth.github.io/comic-mono-font/ComicMono-Bold.ttf
 fc-cache
